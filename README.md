fa344ec1e2198fa7974ddda6ab7339e554560738
Fabian Brandão <fabiancpbm@gmail.com>
2024-08-23 22:02:59 -0300

    adding gitignore

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # bank-importer
+
+## Import the bank transactions
+
+'''bash
+    curl -v POST localhost:8080/triggers  -d '
+    {
+		"basePath":"/Users/fabian.brandao/Documents/patriq/bankimporter/input/",
+		"year":2024,
+		"month":8,
+		"day":1,
+		"bank":"nubank",
+		"account":"154250440",
+		"type":"statement"
+	 }'
+'''
--- bank-importer/main.go
+++ bank-importer/main.go
@@ -20,18 +20,6 @@ func main() {
 	router.Run("localhost:8080")
 }
 
-/*
-	 curl -v POST localhost:8080/triggers  -d '
-	 {
-		"basePath":"/Users/fabian.brandao/Documents/patriq/bankimporter/input/",
-		"year":2024,
-		"month":8,
-		"day":1,
-		"bank":"nubank",
-		"account":"154250440",
-		"type":"statement"
-	 }'
-*/
 func postTriggers(c *gin.Context) {
 	var trigger model.Trigger
 
