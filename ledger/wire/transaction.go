1ff4e552ca3b68cb7f498ba278717d84d626c7c4
Fabian Brandão <fabiancpbm@gmail.com>
2024-09-11 11:46:49 -0300

    :nail_care:

--- ledger/wire/transaction.go
+++
@@ -1,9 +0,0 @@
-package wire
-
-type Transaction struct {
-	ID       string  `json:"id"`
-	SourceID string  `json:"sourceId"`
-	TargetID string  `json:"targetId"`
-	Date     string  `json:"date"`
-	Amount   float32 `json:"amount"`
-}
